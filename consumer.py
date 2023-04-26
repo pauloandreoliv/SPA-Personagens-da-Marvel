795eac9ec7969f55c08b098accb5a8e34c036b6b
Paulo André <pauloandrejanga@gmail.com>
2023-04-26 18:02:53 -0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,6 +1,7 @@
 import requests
 import hashlib
 import time
+import math
 
 private_key = "aee05529d16467018b061c20de02d8873daa6aa9"
 public_key = "a85314be8d4d34bb6e28dc2224274c9b"
@@ -13,22 +14,50 @@ md5.update(string.encode('utf-8'))
 md5_hash = md5.hexdigest()
 
 
-def request(limit,offset):
-    request_url = f"https://gateway.marvel.com:443/v1/public/characters?limit={limit}&offset={offset}&apikey={public_key}&ts={timestamp}&hash={md5_hash}"
+def request(offset):
+    request_url = f"https://gateway.marvel.com:443/v1/public/characters?limit=100&offset={offset}&apikey={public_key}&ts={timestamp}&hash={md5_hash}"
     request_return = requests.get(request_url)
     result = request_return.json()
 
-    results_list = result["data"]
-    return results_list
+    data_list = result["data"]
 
-def listing(limit,offset):
-    results_list = request(limit,offset)
+    return data_list
+
+
+def listing(offset, past_requests, characters):
+    data_list = request(offset)
+
+    results_list = data_list["results"]
+    total = data_list["total"]
+
+    number_requests = total / 100
+    number_requests = math.ceil(number_requests)
+
+    past_requests = past_requests
+    
     k = 0
-    while k < len(results_list):
-        print(k["name"])
-        k += 1
-    if k == 99:
-        k = 0
-        listing(limit+100,offset+100)
 
-listing(0,0)
+    characters = characters
+    
+    while k < len(results_list) and past_requests < number_requests:
+        name = results_list[k]["name"]
+        description = results_list[k]["description"]
+        path_thumbnail = results_list[k]["thumbnail"]["path"]
+        extension_thumbnail = results_list[k]["thumbnail"]["extension"]
+        thumbnail = path_thumbnail + "." + extension_thumbnail
+
+        character = []
+        character.append(name)
+        character.append(description)
+        character.append(thumbnail)
+
+        characters.append(character)
+
+        if k == 99:
+            past_requests += 1
+            offset = past_requests * 100
+            listing(offset, past_requests, characters)
+            
+        k += 1
+        
+listing(0,0, [])
