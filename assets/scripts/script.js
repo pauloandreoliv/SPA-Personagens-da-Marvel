0960a09860c947b84a728cd3340890ac709fcdb9
Paulo André <pauloandrejanga@gmail.com>
2023-04-28 19:48:31 -0300

    Implementando função do script redirecionar()

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,17 @@
 window.onload.redirecionar()
 
 function redirecionar(){
-  console.log("");
+  var caminho = window.location.pathname;
+  
+  if (caminho == "/advance"){
+    window.location.href = "/advance#1"
+  }
+
+  if (caminho == "/list_all"){
+    window.location.href = "/list_all#1"
+  }
+  
+  if (caminho == "/search_for"){
+    window.location.href = "/search_for#2"
+  }
 }
